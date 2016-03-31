4989d2df9cbfae68c590aa0603446a1739f54d8e
Axel Eirola <axel.eirola@futurice.com>
2016-03-31 20:34:34 +0300

    Add configuration

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 var restify = require('restify');
 var botbuilder = require('botbuilder');
+var yargs = require('yargs')
+  .options({
+    'i': {
+      alias: 'id',
+      default: 'botsong',
+      describe: 'appId',
+      type: 'string'
+    },
+    's': {
+      alias: 'secret',
+      demand: true,
+      describe: 'appSecret',
+      type: 'string'
+    },
+    'p': {
+      alias: 'port',
+      default: 3978,
+      descript: 'TCP port',
+      type: 'number'
+    }
+  })
+  .env()
+  .help('h')
+  .alias('h', 'help')
+  .argv;
 
 var botsong = require('./bot.js');
 
 // Create bot and add dialogs
-var bot = new botbuilder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
+var bot = new botbuilder.BotConnectorBot({
+  appId: yargs.id,
+  appSecret: yargs.secret
+});
 bot.add('/', function (session) {
   session.send(botsong.onMessage(session));
 });
@@ -15,7 +43,7 @@ server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 
 if (require.main === module) {
   // Start server
-  server.listen(process.env.port || 3978, function () {
+  server.listen(yargs.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
   });
 }
